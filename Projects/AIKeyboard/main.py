98ee8a054b6085d2777cc0d2e8f2793938362984
mrityunjayshukla411 <mrityunjayshukla411@gmail.com>
2021-08-24 00:43:07 +0530

    AIKeyBoardUpgraded

--- Projects/AIKeyboard/main.py
+++ Projects/AIKeyboard/main.py
@@ -10,9 +10,9 @@ cap.set(3, 1280)
 cap.set(4, 720)
 
 detector = HandDetector(detectionCon=0.8)
-keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P","<-"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
-        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
+        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/",]]
 finalText = ""
 
 keyboard = Controller()
@@ -61,7 +61,7 @@ buttonList = []
 for i in range(len(keys)):
     for j, key in enumerate(keys[i]):
         buttonList.append(Button([100 * j + 50, 100 * i + 50], key))
-
+lastChar = ""
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
@@ -82,12 +82,16 @@ while True:
 
                 ## when clicked
                 if l < 30:
-                    keyboard.press(button.text)
-                    cv2.rectangle(img, button.pos, (x + w, y + h), (0, 255, 0), cv2.FILLED)
-                    cv2.putText(img, button.text, (x + 20, y + 65),
-                                cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
-                    finalText += button.text
-                    sleep(0.15)
+                    if button.text == "<-":
+                        finalText = finalText[:-1]
+                    else:
+                        keyboard.press(button.text)
+                        cv2.rectangle(img, button.pos, (x + w, y + h), (0, 255, 0), cv2.FILLED)
+                        cv2.putText(img, button.text, (x + 20, y + 65),
+                                    cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+                        finalText += button.text
+                        lastChar = button.text
+                        sleep(0.15)
 
     cv2.rectangle(img, (50, 350), (700, 450), (175, 0, 175), cv2.FILLED)
     cv2.putText(img, finalText, (60, 430),
